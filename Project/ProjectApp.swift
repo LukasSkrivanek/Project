f29a4c27407bfb82aea3be43d8d010af770dbadd
LukasSkrivanek <luky.skrivos@gmail.com>
2025-04-14 08:21:26 +0200

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by macbook on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
