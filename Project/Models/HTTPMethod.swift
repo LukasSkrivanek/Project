2cb045b8c14f552001973e818cbdf6c806a2af52
LukasSkrivanek <luky.skrivos@gmail.com>
2025-04-15 14:00:43 +0200

    add networking general endpoint with httpmethods  and errors

---
+++ Project/Models/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Project
+//
+//  Created by macbook on 15.04.2025.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
